d297afbd38cfba04899ceed9f5a264d06c72e69a
World Machine (Skelly) <47392011+vtheskeleton@users.noreply.github.com>
2022-09-21 08:04:39 +0000

    Friend UI

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -58,12 +58,12 @@ router.get("/view/:id", (req : Request, res : Response) => {
 
 router.get("/friends", checkAuth, (req : Request, res : Response) => {
 	let Friends = db.getFriends();
-	Friends.find({$or: [{recipient: req.query.userId}, {requester: req.query.userId}]}, (err, friends) => {
+	Friends.find({$or: [{recipient: res.locals.user._id}, {requester: res.locals.user._id}]}, (err, friends) => {
 		if (err) {
 			errorNotifier(err, JSON.stringify({user: req.user, query: req.query, url: req.url}));
 			return res.status(500).render("error", { title: "Vukkyboxn't :(" });
 		}
-		res.render("friends", { title: "Vukkybox", friends})
+		res.render("friends", { title: "Vukkybox", friendships: friends})
 	})
 })
 
